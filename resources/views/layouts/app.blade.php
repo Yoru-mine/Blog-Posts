454705f27dc09e70a93b7df9abee61bf4287d292
sabin azna <lyunyatut@gmail.com>
2026-07-01 02:34:03 +0500

    design: switch favicon to svg format

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
 
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=1">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.ico.svg') }}">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <script src="https://unpkg.com/lenis@1.1.14/dist/lenis.min.js"></script>
